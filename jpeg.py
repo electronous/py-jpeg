d70701db39c2de96a805c62e94865ceaa3b5a0dc
Brian Armstrong <brian.armstrong.ece@gmail.com>
2011-09-06 18:36:58 -0500

    Add method to skip uninteresting fields, did some cleanup

--- jpeg.py
+++ jpeg.py
@@ -22,49 +22,90 @@ class Jpeg(object):
 
 	# marker codes that identify the various headers in JPEG
 	markers = {
-			'\xd8': 'SOI',
-			'\xe0': 'APP0',
-			'\xdb': 'DQT',
+			# The encoding process is actually stored as part of the SOF marker
+			# For this reason, there are several SOF markers here
 			'\xc0': 'SOF0',
+			'\xc1': 'SOF1',
+			'\xc2': 'SOF2',
+			'\xc3': 'SOF3',
+
+			# We take a break from SOF markers to bring you: DHT
+			# 'DHT' = Define Huffman Tree
 			'\xc4': 'DHT',
-			'\xda': 'SOS',
+
+			# And now return to SOF markers
+			'\xc5': 'SOF5',
+			'\xc6': 'SOF6',
+			'\xc7': 'SOF7',
+			'\xc8': 'SOF_JPEG',
+			'\xc9': 'SOF9',
+			'\xca': 'SOF10',
+			'\xcb': 'SOF11',
+
+			# DAC marker
+			'\xcc': 'DAC',
+
+			# More SOF markers
+			'\xcd': 'SOF13',
+			'\xce': 'SOF14',
+			'\xcf': 'SOF15',
+
+			'\xd8': 'SOI',
 			'\xd9': 'EOI',
+			'\xda': 'SOS',
+			'\xdb': 'DQT',
+
+			'\xe0': 'APP0',
+			'\xe1': 'APP1',
+			'\xe2': 'APP2',
+			'\xe3': 'APP3',
+			'\xe4': 'APP4',
+			'\xe5': 'APP5',
+			'\xe6': 'APP6',
+			'\xe7': 'APP7',
+			'\xe8': 'APP8',
+			'\xe9': 'APP9',
+			'\xea': 'APP10',
+			'\xeb': 'APP11',
+			'\xec': 'APP12',
+			'\xed': 'APP13',
+			'\xee': 'APP14',
+			'\xef': 'APP15',
 	}
 
 	marker_handlers = {}
 
 	# zigzag order to standard (natural) array order lookup tables
-	zigzag_natural = {	2: [0, 1, 8, 9],
-						3: [0, 1, 8, 16, 9, 2, 10, 17, 18],
-						4: [0, 1, 8, 16, 9, 2, 3, 10,
-							17, 24, 25, 18, 11, 19, 26, 27],
-						5: [0, 1, 8, 16, 9, 2, 3, 10,
-							17, 24, 32, 25, 18, 11, 4, 12,
-							19, 26, 33, 34, 27, 20, 28, 35,
-							36],
-						6: [0, 1, 8, 16, 9, 2, 3, 10,
-							17, 24, 32, 25, 18, 11, 4, 5,
-							12, 19, 26, 33, 40, 41, 34, 27,
-							20, 13, 21, 28, 35, 42, 43, 36,
-							29, 37, 44, 45],
-						7: [0, 1, 8, 16, 9, 2, 3, 10,
-							17, 24, 32, 25, 18, 11, 4,  5,
-							12, 19, 26, 33, 40, 48, 41, 34,
-							27, 20, 13, 6, 14, 21, 28, 35,
-							42, 49, 50, 43, 36, 29, 22, 30,
-							37, 44, 51, 52, 45, 38, 46, 53,
-							54],
-						8: [0, 1, 8, 16, 9, 2, 3, 10,
-							17, 24, 32, 25, 18, 11, 4, 5,
-							12, 19, 26, 33, 40, 48, 41, 34,
-							27, 20, 13, 6, 7, 14, 21, 28,
-							35, 42, 49, 56, 57, 50, 43, 36,
-							29, 22, 15, 23, 30, 37, 44, 51,
-							58, 59, 52, 45, 38, 31, 39, 46,
-							53, 60, 61, 54, 47, 55, 62, 63]
+	zigzag_natural = {
+			2: [0, 1, 8, 9],
+			3: [0, 1, 8, 16, 9, 2, 10, 17, 18],
+			4: [0, 1, 8, 16, 9, 2, 3, 10,
+				17, 24, 25, 18, 11, 19, 26, 27],
+			5: [0, 1, 8, 16, 9, 2, 3, 10,
+				17, 24, 32, 25, 18, 11, 4, 12,
+				19, 26, 33, 34, 27, 20, 28, 35,
+				36],
+			6: [0, 1, 8, 16, 9, 2, 3, 10,
+				17, 24, 32, 25, 18, 11, 4, 5,
+				12, 19, 26, 33, 40, 41, 34, 27,
+				20, 13, 21, 28, 35, 42, 43, 36,
+				29, 37, 44, 45],
+			7: [0, 1, 8, 16, 9, 2, 3, 10,
+				17, 24, 32, 25, 18, 11, 4,  5,
+				12, 19, 26, 33, 40, 48, 41, 34,
+				27, 20, 13, 6, 14, 21, 28, 35,
+				42, 49, 50, 43, 36, 29, 22, 30,
+				37, 44, 51, 52, 45, 38, 46, 53,
+				54],
+			8: [0, 1, 8, 16, 9, 2, 3, 10,
+				17, 24, 32, 25, 18, 11, 4, 5,
+				12, 19, 26, 33, 40, 48, 41, 34,
+				27, 20, 13, 6, 7, 14, 21, 28,
+				35, 42, 49, 56, 57, 50, 43, 36,
+				29, 22, 15, 23, 30, 37, 44, 51,
+				58, 59, 52, 45, 38, 31, 39, 46,
+				53, 60, 61, 54, 47, 55, 62, 63]
 	}
-
-
 
 
 
@@ -85,6 +126,9 @@ class Jpeg(object):
 
 		# Attributes gathered from DQT header
 		self.quantization_tables = [[], [], [], []]
+		self.quantization_high_precision = [False, False, False, False]
+
+		# Attributes gathered from SOF header
 
 		self.build_from_buf()
 
@@ -116,7 +160,33 @@ class Jpeg(object):
 		handler = Jpeg.marker_handlers.get(marker)
 		if handler is None:
 			raise MarkerNotHandledError(marker)
+		tracker = self.trackers.get(marker, [])
+		tracker.append(self._index)
+		self.trackers[marker] = tracker
 		return handler(self)
+
+	# We define this next function for headers which we don't care about or can't do anything with
+	#	but need to skip over anyway
+	def handle_uninteresting_variable_length_header(self):
+		index = self._index
+		length = struct.unpack('>H', self._buf[index:index+2])[0]
+		index += length
+		self._index = index
+
+	marker_handlers['APP1'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP2'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP3'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP4'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP5'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP6'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP7'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP8'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP9'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP10'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP11'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP12'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP13'] = handle_uninteresting_variable_length_header
+	marker_handlers['APP15'] = handle_uninteresting_variable_length_header
 
 	def handle_soi(self):
 		### no need to increase self._index here because soi is a 0-length header
@@ -130,10 +200,9 @@ class Jpeg(object):
 		INTERESTING_LEN = 14 # does not include 2-byte length field
 		JFIF_IDENT = 'JFIF\x00'
 
-		self.trackers['SOI'] = self._index
 		index = self._index
+
 		# first get a 2-byte length field
-		length_slice = self._buf[index:index + 2]
 		length = struct.unpack('>H', self._buf[index:index + 2])[0]
 
 		index += 2
@@ -181,11 +250,11 @@ class Jpeg(object):
 
 	marker_handlers['APP0'] = handle_app0
 
+
 	def handle_dqt(self):
 		# The DQT header contains one of the quantization tables used to encode the JPEG
 		# These tables are needed to perform the IDCT
 		# One header <--> one table (can have MAX_QUANTIZATION_TABLES)
-		self.trackers['DQT'] = self._index
 		index = self._index
 
 		length = struct.unpack('>H', self._buf[index:index+2])[0]
@@ -199,8 +268,6 @@ class Jpeg(object):
 		quant_precision = quant_num_and_prec >> 4
 		if quant_num >= self.MAX_QUANTIZATION_TABLES:
 			raise BadFieldError('DQT')
-
-		table = self.quantization_tables[quant_num]
 
 		# Get the number of entries present for the quant. table.
 		# Subtract 3 to remove the previous fields' lengths and then div by 2 for higher precision table
@@ -239,9 +306,17 @@ class Jpeg(object):
 				table[zigzag_natural[i]] = entry
 				index += 1
 
+		self.quantization_tables[quant_num] = table
+		self.quantization_high_precision[quant_num] = quant_precision
 		self._index = index
 
 	marker_handlers['DQT'] = handle_dqt
+
+	def handle_sof0(self):
+
+		self._index = index
+
+	marker_handlers['SOF0'] = handle_sof0
 
 
 class Foo(object):
